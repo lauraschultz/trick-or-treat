ac6b62954f7ca3e088958a22cf958dffd9838d61
lauraschultz <schultzlaurac@gmail.com>
2020-10-21 19:42:38 -0500

    remove directory

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,8 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/trickOrTreat">
+    <BrowserRouter>
+    {/* basename="/trickOrTreat" */}
       <App />
     </BrowserRouter>
   </React.StrictMode>,
